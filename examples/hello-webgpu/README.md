3d9f855aa5f5ef65c673ef15aaa84fe9037f4432
Alexander Schumacher <6401757+story75@users.noreply.github.com>
2024-12-29 14:47:24 +0100

    feat: add hello webgpu example

---
+++ examples/hello-webgpu/README.md
@@ -0,0 +1,8 @@
+# Hello World with raw WebGPU
+
+An example to show the basics of raw WebGPU with TypeScript. This example is a simple "Hello World" program that draws a red square to the screen. it should be a reference for how to get started with WebGPU in the most basic way.
+
+## License
+
+This package is part of the Toasty Engine project and is licensed under the MIT License.
+
---
+++ examples/hello-webgpu/src/main.ts
@@ -0,0 +1,91 @@
+/// <reference types="@webgpu/types" />
+import './style.css';
+
+const canvas = document.querySelector('canvas');
+if (!canvas) {
+  throw new Error('Canvas not found');
+}
+const context = canvas.getContext('webgpu');
+if (!context) {
+  throw new Error('WebGPU context not found');
+}
+
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) {
+  throw new Error('Could not request WebGPU adapter!');
+}
+
+const device = await adapter.requestDevice();
+
+context.configure({
+  device,
+  format: presentationFormat,
+  alphaMode: 'premultiplied',
+});
+
+const devicePixelRatio = window.devicePixelRatio;
+canvas.width = window.innerWidth * devicePixelRatio;
+canvas.height = window.innerHeight * devicePixelRatio;
+
+const render = () => {
+  const shaderModule = device.createShaderModule({
+    code: `
+      @vertex
+      fn vs_main(@builtin(vertex_index) vertex_index: u32) -> @builtin(position) vec4f {
+        const pos = array(
+          vec2(-0.5, -0.5), vec2(0.5, -0.5), vec2(-0.5, 0.5),
+          vec2(-0.5, 0.5), vec2(0.5, -0.5), vec2(0.5, 0.5),
+        );
+      
+        return vec4f(pos[vertex_index], 0.0, 1.0);
+      }
+
+      @fragment
+      fn fs_main() -> @location(0) vec4f {
+        return vec4f(1.0, 0.0, 0.0, 1.0);
+      }
+    `,
+  });
+
+  const pipelineLayout = device.createPipelineLayout({
+    bindGroupLayouts: [],
+  });
+
+  const pipeline = device.createRenderPipeline({
+    layout: pipelineLayout,
+    vertex: {
+      module: shaderModule,
+      entryPoint: 'vs_main',
+    },
+    fragment: {
+      module: shaderModule,
+      entryPoint: 'fs_main',
+      targets: [{ format: presentationFormat }],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  });
+
+  const commandEncoder = device.createCommandEncoder();
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    colorAttachments: [
+      {
+        view: context.getCurrentTexture().createView(),
+        clearValue: { r: 0.8, g: 0.8, b: 0.8, a: 1.0 },
+        loadOp: 'clear',
+        storeOp: 'store',
+      },
+    ],
+  };
+
+  const renderPass = commandEncoder.beginRenderPass(renderPassDescriptor);
+  renderPass.setPipeline(pipeline);
+  renderPass.draw(6);
+  renderPass.end();
+  device.queue.submit([commandEncoder.finish()]);
+};
+
+render();
